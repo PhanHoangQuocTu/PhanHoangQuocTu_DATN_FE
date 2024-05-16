91da97e9932cc1b66ee232897e7e4d77090a4654
Tu <tuphan694@gmail.com>
2024-05-16 22:41:14 +0700

    fix: deploy

--- src/pages/books/[bookId]/index.tsx
+++ src/pages/books/[bookId]/index.tsx
@@ -1,3 +1,11 @@
 import BookDetailPage from '@/modules/BookDetailPage';
 
 export default BookDetailPage;
+
+export async function getServerSideProps({ params }: any) {
+  return {
+    props: {
+      bookId: params.bookId,
+    },
+  };
+}
